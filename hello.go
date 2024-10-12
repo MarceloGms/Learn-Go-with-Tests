26e38a22cf91a3f1022498c3058cd4dbc7e271b4
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-10-12 19:04:29 +0100

    refactor

--- hello.go
+++ hello.go
@@ -2,24 +2,35 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const ( // can define multiple variable/constants using parentheses
+	spanish = "Spanish"
+	french = "French"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+)
 
-func Hello(name string, language string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	return greetingPrefix(language) + name
+}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
+// in go, functions starting with lowercase letters are PRIVATE, and uppercase are PUBLIC.
+// this function is private because we don't want the internals of our algorithm exposed to the world.
+func greetingPrefix(language string) (prefix string) { //! named return value (prefix string).
+	// the value of prefix will be set to "" (str) or 0 (int) automatically by default
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+
+	return // just using return will return prefix
 }
 
 func main() {
