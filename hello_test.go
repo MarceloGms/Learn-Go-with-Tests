bdf48551eaac0964e705c47c18e9a43ba7156030
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-10-12 16:46:27 +0100

    refactor tests

--- hello_test.go
+++ hello_test.go
@@ -7,17 +7,19 @@ func TestHello(t *testing.T) {
 		got := Hello("Marcelo")
 		want := "Hello, Marcelo!"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		AssertCorrectMessage(t, got, want)
 	})
 	t.Run("Say 'Hello, world!' if empty string is provided", func (t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		AssertCorrectMessage(t, got, want)
 	})
-	
+}
+
+func AssertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
