4c51f3b2e96b53af413cb65381cd07ad73034fc2
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-10-12 16:37:26 +0100

    implement empty string feature

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Marcelo")
-	want := "Hello, Marcelo!"
+	t.Run("Say Hello to someone", func(t *testing.T) {
+		got := Hello("Marcelo")
+		want := "Hello, Marcelo!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("Say 'Hello, world!' if empty string is provided", func (t *testing.T) {
+		got := Hello("")
+		want := "Hello, World!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	
 }
