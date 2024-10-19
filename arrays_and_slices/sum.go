15cd73e97842a0b964d303bf70fce8e3477628ac
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-10-19 19:28:15 +0100

    sum all

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -1,8 +1,20 @@
 package arrays_and_slices
 
-func Sum(arr [5]int) (sum int) {
-	for i := 0; i < len(arr); i++ {
-		sum += arr[i]
+func Sum(arr []int) (sum int) {
+	for _, n := range arr {
+		sum += n
 	}
+	/* for i := 0; i < len(arr); i++ {
+		sum += arr[i]
+	} */
 	return
+}
+
+func SumAll(numbers ...[]int) []int {
+	sums := make([]int, len(numbers))
+
+	for i, slice := range numbers {
+		sums[i] = Sum(slice)
+	}
+	return sums
 }
--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,14 +1,41 @@
 package arrays_and_slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	/* t.Run("Array", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	expected := 15
+		got := Sum(numbers)
+		expected := 15
 
+		AssertCorrectMessage(t, got, expected, numbers)
+	}) */
+	t.Run("Slice", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		expected := 6
+
+		AssertCorrectMessage(t, got, expected, numbers)
+	})
+}
+
+func AssertCorrectMessage(t testing.TB, got, expected int, numbers []int) {
+	t.Helper()
 	if got != expected {
 		t.Errorf("got %d, but expected %d given %v", got, expected, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1,2}, []int{0,9})
+	want := []int{3,9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, but expected %v", got, want)
 	}
 }
