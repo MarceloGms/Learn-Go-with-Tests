5101cc3d584fa1f9bab724d476c63c293346126a
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-10-27 21:06:49 +0000

    Refactor shape calculations and add test cases for different shapes

--- structs_methods_interfaces/rectangle.go
+++ structs_methods_interfaces/rectangle.go
@@ -1,9 +1,37 @@
 package structsmethodsinterfaces
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Shape interface {
+	Area() float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
 }
