365e0407e8ad1fead39e2af70b022142f864c09f
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-10-28 11:00:00 +0000

    add bank

---
+++ pointers_and_errors/bank.go
@@ -0,0 +1,31 @@
+package pointersanderrors
+
+import "fmt"
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin //! lowercase variable so its private outside the package
+	// only these methods can manipulate this variable, but the struct is public
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
---
+++ pointers_and_errors/bank_test.go
@@ -0,0 +1,34 @@
+package pointersanderrors
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+	
+		got := wallet.Balance()
+		fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+		want := Bitcoin(10)
+	
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+	
+		got := wallet.Balance()
+		fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+		want := Bitcoin(10)
+	
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+	
+}
