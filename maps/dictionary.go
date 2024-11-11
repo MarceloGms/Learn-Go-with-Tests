9ce6a624b8784527623f0b38c4658e2e835452c8
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-11-11 18:56:23 +0000

    dictionary, unknown word

---
+++ maps/dictionary.go
@@ -0,0 +1,15 @@
+package maps
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(keyword string) (string, error) {
+	definition, ok := d[keyword]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
